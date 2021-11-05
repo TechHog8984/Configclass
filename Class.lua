2308b683ee9b8e85e8125f01bdda2d6261205d54
TechHog <77560013+TechHog8984@users.noreply.github.com>
2021-11-04 21:42:59 -0400

    Update Class.lua

--- Class.lua
+++ Class.lua
@@ -2,29 +2,42 @@ local class = {}
 local httpservice = game:GetService'HttpService'
 
 function class:CreateConfig(filename, foldername)
-	local filepath = filename
-	if foldername then
-		filepath = foldername .. '/' .. filename
+	if filename then
+		local filepath = filename
+		if foldername then
+			filepath = foldername .. '/' .. filename
+			if not isfolder(foldername) then
+				makefolder(foldername)
+			end
+		end
+
+		local config = {}
+
+		local function update()
+			writefile(filepath, httpservice:JSONEncode(config))
+		end
+
+		return setmetatable({}, {
+			__index = function(self, index)
+				return config[index]
+			end,
+			__newindex = function(self, index, value)
+				config[index] = value
+				update()
+			end,
+		}), filepath
+	else
+		return 'filename is required'
 	end
-
-	local config = {}
-
-	local function update()
-		writefile(filepath, httpservice:JSONEncode(config))
-	end
-
-	return setmetatable({}, {
-		__index = function(self, index)
-			return config[index]
-		end,
-		__newindex = function(self, index, value)
-			config[index] = value
-			update()
-		end,
-	}), filepath
+	return 'error'
 end
 function class:GetConfig(filepath)
-	return httpservice:JSONDecode(readfile(filepath)) or nil
+	local success, config = pcall(function() return httpservice:JSONDecode(readfile(filepath)) end)
+	if success then
+		return config
+	else
+		return nil
+	end
 end
 
 return class
