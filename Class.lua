552d15fa203d223feb4702f534f34fa511a31357
TechHog <77560013+TechHog8984@users.noreply.github.com>
2021-11-04 21:23:21 -0400

    Create Class.lua

---
+++ Class.lua
@@ -0,0 +1,27 @@
+local class = {}
+local httpservice = game:GetService'HttpService'
+
+function class:CreateConfig(filename, foldername)
+	local filepath = filename
+	if foldername then
+		filepath = foldername .. '/' .. filename
+	end
+
+	local config = {}
+
+	local function update()
+		writefile(filepath, httpservice:JSONEncode(config))
+	end
+
+	return setmetatable({}, {
+		__index = function(self, index)
+			return config[index]
+		end,
+		__newindex = function(self, index, value)
+			config[index] = value
+			update()
+		end,
+	})
+end
+
+return class
